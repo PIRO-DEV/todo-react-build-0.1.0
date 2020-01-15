b5a131f218c1215b47c4afa18ae4d9f998560072
Pradhyumna Kumar Kandamuru <pradhyumnakandamuru@gmail.com>
2020-01-16 00:07:29 +0530

    v0.1.0-frontend-build

---
+++ precache-manifest.482bc11c07f93a3128d0d84cadcd755f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b748268da603b1bbfce322ecdb3de6e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8a92ab5f1b84d7c38af",
+    "url": "/static/css/main.d03f01ea.chunk.css"
+  },
+  {
+    "revision": "041c09173098be979c3b",
+    "url": "/static/js/2.1432d27a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.1432d27a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8a92ab5f1b84d7c38af",
+    "url": "/static/js/main.b0d93a73.chunk.js"
+  },
+  {
+    "revision": "92ee27872d43b18cf475",
+    "url": "/static/js/runtime-main.09b07318.js"
+  }
+]);
